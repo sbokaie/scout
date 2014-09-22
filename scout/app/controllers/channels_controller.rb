557a562aa142215f21492433f0f824dd6cc0e13c
sbokaie <shawn.bokaie@gmail.com>
2014-09-22 16:33:33 -0700

    more YouTube API

--- scout/app/controllers/channels_controller.rb
+++ scout/app/controllers/channels_controller.rb
@@ -3,5 +3,17 @@ class ChannelsController < ApplicationController
     request = Typhoeus.get(
       "https://www.googleapis.com/youtube/v3/channels?part=contentDetails")
     @results = JSON.parse(request.body)
+
+    @all_channels = []
+    @channels = Channel.all
+
+    @channels.each do |channel|
+      new_channel = {}
+      new_channel["channel"] = channel
+
+    @all_channels.push new_channel
+
   end
+
+  
 end
